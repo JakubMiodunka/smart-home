549c1c6c97a03da9113605d67b36f48531a8a1a6
JakubMiodunka <miodunkajakub@gmail.com>
2026-04-07 13:42:14 +0200

    #23 Adding custom mock of DelegatingHandler class to be able to use HTTP client in unit tests.

---
+++ src/Server/UnitTests/FakeDelegatingHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace UnitTests;
+
+// TODO: rework
+public class FakeDelegatingHandler : DelegatingHandler
+{
+    private readonly Func<HttpRequestMessage, HttpResponseMessage>? _requestHandler;
+
+    public bool WasDataSent { get; private set; }
+
+    public FakeDelegatingHandler(
+        Func<HttpRequestMessage, HttpResponseMessage>? requestHandler = null)
+    {
+        ArgumentNullException.ThrowIfNull(requestHandler);
+
+        _requestHandler = requestHandler;
+        WasDataSent = false;
+    }
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken _)
+    {
+        WasDataSent = true;
+
+        return _requestHandler is null
+            ? Task.FromException<HttpResponseMessage>(new NotImplementedException("Request handler unspecified:"))
+            : Task.FromResult(_requestHandler.Invoke(request));
+    }
+}
