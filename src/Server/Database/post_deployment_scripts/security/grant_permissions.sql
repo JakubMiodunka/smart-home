36b45d36bab17019109f6c62b6af66085d82c8e4
JakubMiodunka <miodunkajakub@gmail.com>
2026-05-11 20:34:46 +0200

    [#34] Adding SQL script, which grants EXECUTE permissions to app user.

---
+++ src/Server/Database/post_deployment_scripts/security/grant_permissions.sql
@@ -0,0 +1,13 @@
+﻿PRINT '[INFO] Granting permissions: PermissionsType=[EXECUTE], UserName=[$(APP_USER_NAME)]';
+
+IF EXISTS (SELECT * FROM sys.database_principals WHERE name = '$(APP_USER_NAME)')
+BEGIN
+    GRANT EXECUTE TO [$(APP_USER_NAME)];
+    
+    PRINT '[INFO] Permissions granted successfully: PermissionsType=[EXECUTE], UserName=[$(APP_USER_NAME)]';
+END
+ELSE
+BEGIN
+    PRINT '[WARNING] Failed to grant permissions: Message=[User not found.], PermissionsType=[EXECUTE], UserName=[$(APP_USER_NAME)]';
+END
+GO
