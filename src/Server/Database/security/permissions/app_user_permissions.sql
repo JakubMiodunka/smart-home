58f5894649f8ec30a89790d4e2bd27fffafed545
JakubMiodunka <miodunkajakub@gmail.com>
2026-05-14 21:55:35 +0200

    [#34] Adding post-deployment SQL script. Adding automation for SQL database user creation along with permissions assignment.

---
+++ src/Server/Database/security/permissions/app_user_permissions.sql
@@ -0,0 +1,13 @@
+﻿PRINT '[INFO] Granting permissions: PermissionsType=[EXECUTE], UserName=[$(AppUserName)]';
+
+IF EXISTS (SELECT 1 FROM sys.database_principals WHERE name = '$(AppUserName)')
+BEGIN
+    GRANT EXECUTE TO [$(AppUserName)];
+    
+    PRINT '[INFO] Permissions granted successfully: PermissionsType=[EXECUTE], UserName=[$(AppUserName)]';
+END
+ELSE
+BEGIN
+    PRINT '[WARNING] Failed to grant permissions: Message=[User not found.], PermissionsType=[EXECUTE], UserName=[$(AppUserName)]';
+END
+GO
