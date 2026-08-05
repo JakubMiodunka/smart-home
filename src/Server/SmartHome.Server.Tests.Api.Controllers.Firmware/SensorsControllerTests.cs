8dc60e073d449548e4d84ed55f96235f315b343c
JakubMiodunka <miodunkajakub@gmail.com>
2026-08-05 19:13:35 +0200

    [#33] Adding unit tests for firmware API sensors controller.

--- src/Server/SmartHome.Server.Tests.Api.Controllers.Firmware/SensorsControllerTests.cs
+++ src/Server/SmartHome.Server.Tests.Api.Controllers.Firmware/SensorsControllerTests.cs
@@ -185,15 +185,148 @@ internal sealed class SensorsControllerTests
     }
 
     [Test]
-    public async Task RegistrationOfKnownSwitchCausesReturnOfItsExpectedState() =>
-        throw new NotImplementedException();
+    public async Task RegistrationOfKnownSwitchCausesReturnOfItsDetails()
+    {
+        Randomizer randomizer = TestContext.CurrentContext.Random;
+
+        StationEntity parentStationEntity = randomizer.NextOnlineStationEntity();
+
+        Mock<IHttpContextAccessor> httpContextAccessorStub =
+            FakeDataGenerationUtilities.CreateHttpContextAccessorFake(parentStationEntity.IpAddress);
+
+        var stationsRepositoryMock = new Mock<IStationsRepository>();
+
+        stationsRepositoryMock.Setup(mock =>
+            mock.GetSingleStationAsync(
+                filterById: It.IsAny<bool>(),
+                id: It.IsAny<long?>(),
+                filterByIpAddress: true,
+                ipAddress: parentStationEntity.IpAddress,
+                filterByMacAddress: It.IsAny<bool>(),
+                macAddress: It.IsAny<PhysicalAddress?>()))
+            .ReturnsAsync(parentStationEntity);
+
+        SensorEntity sensorEntity = randomizer.NextSensorEntity() with
+        {
+            StationId = parentStationEntity.Id
+        };
+
+        var sensorsRepositoryMock = new Mock<ISensorsRepository>();
+
+        sensorsRepositoryMock.Setup(mock => mock
+            .GetSingleSensorAsync(
+                filterById: false,
+                id: null,
+                filterByStationId: true,
+                stationId: sensorEntity.StationId,
+                filterByLocalId: true,
+                localId: sensorEntity.LocalId))
+            .ReturnsAsync(sensorEntity);
+
+        var loggerMock = new FakeLogger<SensorsController>();
+
+        var controllerUnderTest = new SensorsController(
+            httpContextAccessorStub.Object,
+            sensorsRepositoryMock.Object,
+            stationsRepositoryMock.Object,
+            loggerMock);
+
+        var request = new SensorRegistrationRequest(sensorEntity.LocalId, sensorEntity.MeasurementType);
+        IActionResult response = await controllerUnderTest.RegisterSensor(request);
+
+        var expectedResponse = new SensorRegistrationResponse(sensorEntity.Id);
+        response.AssertOkObjectResult(expectedValue: expectedResponse);
+
+        sensorsRepositoryMock.Verify(mock => mock
+            .GetSingleSensorAsync(
+                filterById: false,
+                id: null,
+                filterByStationId: true,
+                stationId: sensorEntity.StationId,
+                filterByLocalId: true,
+                localId: sensorEntity.LocalId),
+            Times.Once);
+
+        sensorsRepositoryMock.AssertNoContentModifications();
+        stationsRepositoryMock.AssertNoContentModifications();
+
+        IReadOnlyList<FakeLogRecord> logMessages = loggerMock.Collector.GetSnapshot();
+        Assert.That(logMessages, Is.Not.Empty);
+        Assert.That(logMessages, Has.Some.Matches<FakeLogRecord>(record => record.Level == LogLevel.Information));
+        Assert.That(logMessages, Has.None.Matches<FakeLogRecord>(record => LogLevel.Information < record.Level));
+    }
 
     [Test]
-    public async Task RegistrationReturnsBadRequestIfStationIpAddressCannotBeDetermined() =>
-        throw new NotImplementedException();
+    public async Task RegistrationReturnsBadRequestIfStationIpAddressCannotBeDetermined()
+    {
+        Randomizer randomizer = TestContext.CurrentContext.Random;
+
+        Mock<IHttpContextAccessor> httpContextAccessorStub =
+            FakeDataGenerationUtilities.CreateHttpContextAccessorFake(remoteIpAddress: null);
+
+        var stationsRepositoryMock = new Mock<IStationsRepository>();
+        var sensorsRepositoryMock = new Mock<ISensorsRepository>();
+        var loggerMock = new FakeLogger<SensorsController>();
+
+        var controllerUnderTest = new SensorsController(
+            httpContextAccessorStub.Object,
+            sensorsRepositoryMock.Object,
+            stationsRepositoryMock.Object,
+            loggerMock);
+
+        SensorEntity sensorEntity = randomizer.NextSensorEntity();
+        var request = new SensorRegistrationRequest(sensorEntity.LocalId, sensorEntity.MeasurementType);
+        IActionResult response = await controllerUnderTest.RegisterSensor(request);
+
+        response.AssertBadRequestResult();
+
+        sensorsRepositoryMock.AssertNoContentModifications();
+        stationsRepositoryMock.AssertNoContentModifications();
+
+        IReadOnlyList<FakeLogRecord> logMessages = loggerMock.Collector.GetSnapshot();
+        Assert.That(logMessages, Is.Not.Empty);
+        Assert.That(logMessages, Has.Some.Matches<FakeLogRecord>(record => record.Level == LogLevel.Warning));
+        Assert.That(logMessages, Has.None.Matches<FakeLogRecord>(record => LogLevel.Warning < record.Level));
+    }
 
     [Test]
-    public async Task RegistrationReturnsNotFoundIfStationIsNotRegistered() =>
-        throw new NotImplementedException();
+    public async Task RegistrationReturnsNotFoundIfStationIsNotRegistered()
+    {
+        Randomizer randomizer = TestContext.CurrentContext.Random;
+
+        StationEntity parentStationEntity = randomizer.NextOnlineStationEntity();
+
+        Mock<IHttpContextAccessor> httpContextAccessorStub =
+            FakeDataGenerationUtilities.CreateHttpContextAccessorFake(parentStationEntity.IpAddress);
+
+        var stationsRepositoryMock = new Mock<IStationsRepository>();
+        var sensorsRepositoryMock = new Mock<ISensorsRepository>();
+        var loggerMock = new FakeLogger<SensorsController>();
+
+        var controllerUnderTest = new SensorsController(
+            httpContextAccessorStub.Object,
+            sensorsRepositoryMock.Object,
+            stationsRepositoryMock.Object,
+            loggerMock);
+
+        SensorEntity sensorEntity = randomizer.NextSensorEntity() with
+        {
+            StationId = parentStationEntity.Id
+        };
+
+        var request = new SensorRegistrationRequest(sensorEntity.LocalId, sensorEntity.MeasurementType);
+        IActionResult response = await controllerUnderTest.RegisterSensor(request);
+
+        var expectedResponse = new SensorRegistrationResponse(sensorEntity.Id);
+        response.AssertNotFoundResult();
+
+        sensorsRepositoryMock.AssertNoContentModifications();
+        stationsRepositoryMock.AssertNoContentModifications();
+
+        IReadOnlyList<FakeLogRecord> logMessages = loggerMock.Collector.GetSnapshot();
+        Assert.That(logMessages, Is.Not.Empty);
+        Assert.That(logMessages, Has.Some.Matches<FakeLogRecord>(record => record.Level == LogLevel.Warning));
+        Assert.That(logMessages, Has.None.Matches<FakeLogRecord>(record => LogLevel.Warning < record.Level));
+    }
     #endregion
 }
